5c8d41006e42d9bc54fa7763a956697b78dc5dbb
Aliyah <Aliyah@10.0.0.213>
2020-03-04 21:02:25 -0800

    Initial Local Creation

---
+++ TestGit/src/ap/gittest/one/ClassOne.java
@@ -0,0 +1,14 @@
+package ap.gittest.one;
+
+public class ClassOne {
+	
+	public static void main(String args[]){  
+	     System.out.println("Hello Java");  
+	    }  
+	
+	public void method1() {
+		int one =1;
+		int two = 2;
+	}
+
+}
